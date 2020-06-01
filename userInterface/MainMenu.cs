0587d5823b731d6dd94b6005d900241566cf7cbd
gomezLF <gomez_pipe99@hotmail.com>
2020-05-31 19:56:55 -0500

    Creacion del proyecto, con la clase de la ventana principal

---
+++ userInterface/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace userInterface
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
